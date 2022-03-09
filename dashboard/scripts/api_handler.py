a95b3badbd203f0c004c2fe38959496bac1652cb
suarez_work_win <cubin4ik@mail.ru>
2022-03-09 21:44:57 +0300

    api_nadler cleaned up code

--- dashboard/scripts/api_handler.py
+++ dashboard/scripts/api_handler.py
@@ -1,8 +1,14 @@
-from urllib import request, parse, error
+# Module to quickly fetch API data from any source given
+# using just a link provided and parameters
+
+from urllib import request, parse
 from collections import namedtuple
 from datetime import datetime
 from xml.etree import ElementTree
 import json
+
+# TODO: Add key verification
+# TODO: Add multi-query request (urllib.parse.encode)
 
 
 class RequestAPI(object):
@@ -33,28 +39,14 @@ class RequestAPI(object):
     def get_resp(self):
         """Connects and pulls xml data"""
 
-        try:
-            data = request.urlopen(self.url)
-        except error.URLError:
-            proxy_support = request.ProxyHandler(
-                {
-                    'http': 'rb-proxy-de.bosch.com:8080',
-                    'https': 'rb-proxy-special.bosch.com:8080'
-                }
-            )
-            opener = request.build_opener(proxy_support)
-            request.install_opener(opener)
-            data = request.urlopen(self.url)
-
-        # TODO: Delete the above proxy code and uncomment the below line
-        # data = request.urlopen(self.url)
+        data = request.urlopen(self.url)
         enc = data.headers.get_content_charset()
 
         return data.read().decode(enc)
 
 
 class ParseAPI(RequestAPI):
-    """Parsing any API response"""
+    """Parsing any API response using parent module to fetch data silently"""
 
     def __init__(self, api, **kwargs):
         super().__init__(api, **kwargs)
@@ -75,35 +67,28 @@ class ParseAPI(RequestAPI):
         return json.loads(self.resp)
 
 
-def processor():
-    """Apps main controller"""
+def example():
+    """Example of usage"""
 
-    # source_api = 'https://jsonplaceholder.typicode.com/comments'
     source_api = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=02/03/2002'
-    par = 'date_req'
-    value = datetime.today().strftime('%d/%m/%Y')
+    par_name = 'date_req'
+    par_value = datetime.today().strftime('%d/%m/%Y')
 
-    pars = {
-        'query': f'{par}={value}'
-    }
-
-    my_req = ParseAPI(source_api, **pars)
-    # my_req = ParseAPI(source_api)
+    pars = {'query': f'{par_name}={par_value}'}
+    my_req = ParseAPI(source_api, **pars)  # a single line to get data from API
 
     # For XML data type processing
-
     print(my_req.root.tag, my_req.root.attrib)
-
     for child in my_req.root:
         currency = child.find('CharCode').text
-        value = child.find('Value').text
-        print(f'{currency}: {value}')
+        par_value = child.find('Value').text
+        print(f'{currency}: {par_value}')
         print('-' * 20)
 
-    # For JSON data processing
+    # For JSON data type processing use the code below
     # for data in my_req.root:
     #     print(data['postId'])
 
 
 if __name__ == '__main__':
-    processor()
+    example()
