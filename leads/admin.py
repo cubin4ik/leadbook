391deb78c6de421fa9bb9e8bd23a52ddfad68bea
suarez_home_win <cubin4ik@mail.ru>
2022-06-05 21:53:36 +0300

    phone and email added to person admin panel

--- leads/admin.py
+++ leads/admin.py
@@ -2,10 +2,22 @@ from django.contrib import admin
 from .models import LegalForm, Company, Person, Email, Phone, Address
 
 
+class PhoneInline(admin.TabularInline):
+    model = Phone
+    extra = 1
+
+
+class EmailInline(admin.TabularInline):
+    model = Email
+    extra = 1
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [PhoneInline, EmailInline]
+
+
 admin.site.register(LegalForm)
 admin.site.register(Company)
 # admin.site.register(CompanyStatus)
-admin.site.register(Person)
-admin.site.register(Email)
-admin.site.register(Phone)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Address)
